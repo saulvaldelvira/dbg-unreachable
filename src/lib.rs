b3bad4dbad204fbc958c7176c370e4f45c71f10e
Saúl Valdelvira <saul@saulv.es>
2024-07-11 14:58:41 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
+//! This crate defines an [unreachable!] macro that is conditionally
+//! compiled depending on the build profile.
+//!
+//! If the build is **debug** , it translates to [core::unreachable]
+//!
+//! If the build is **release**, it translates to [core::hint::unreachable_unchecked]
+//!
+//! There are cases where [unreachable_unchecked](core::hint::unreachable_unchecked)
+//! is faster that [unreachable](core::unreachable).
+//! This macro uses the unchecked version on release mode, but still checks on debug
+//! mode, allowing you to catch cases in which the unreachable code is reached.
+//!
+//! # Example
+//! ```
+//! use dbg_unreachable::unreachable;
+//!
+//! let a = Some(12_i32);
+//! match a {
+//!     Some(n) if n >= 0 => {},
+//!     Some(n) if n < 0 => {},
+//!     Some(_) => unreachable!("The two arms above cover all possible cases"),
+//!     None => {},
+//! }
+//! ```
+//! The piece of code above translates to this two pieces.
+//!
+//! **Debug**
+//! ```
+//! use dbg_unreachable::unreachable;
+//!
+//! let a = Some(12_i32);
+//! match a {
+//!     Some(n) if n >= 0 => {},
+//!     Some(n) if n < 0 => {},
+//!     Some(_) => core::unreachable!("The two arms above cover all possible cases"),
+//!     None => {},
+//! }
+//! ```
+//!
+//! **Release**
+//! ```
+//! use dbg_unreachable::unreachable;
+//!
+//! let a = Some(12_i32);
+//! match a {
+//!     Some(n) if n >= 0 => {},
+//!     Some(n) if n < 0 => {},
+//!     Some(_) => unsafe { core::hint::unreachable_unchecked() },
+//!     None => {},
+//! }
+//! ```
+
 #![no_std]
 
 #[macro_export]
@@ -26,14 +78,14 @@ mod test {
         match a {
             Some(n) if n >= 0 => {},
             Some(n) if n < 0 => {},
-            Some(_) => unreachable!("The above arms cover all possible cases"),
+            Some(_) => unreachable!("The two arms above cover all possible cases"),
             None => {},
         }
     }
 
     #[test]
     #[should_panic(expected = "internal error: entered unreachable code")]
-    /* In release mode, reaching this macro leads to UB  */
+    /* In release mode, reaching this macro leads to UB */
     #[cfg(debug_assertions)]
     fn will_panic() {
         unreachable!();
